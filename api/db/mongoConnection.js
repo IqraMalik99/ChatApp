d859b448ebbd843d77f1b5f39ac3567620624144
iqra <im4356927@gmail.com>
2025-01-15 14:25:53 -0800

    first

--- api/db/mongoConnection.js
+++ api/db/mongoConnection.js
@@ -4,7 +4,7 @@ let MONGODB=process.env.MONGODB;
 export const mongoConnection= async()=>{
     try{
         // let password = OjjI93O8KW1oCWdD
- let connect= await mongoose.connect(MONGODB);
+ let connect= await mongoose.connect("mongodb+srv://im4356927:OjjI93O8KW1oCWdD@cluster0.dh7sx.mongodb.net/chatApp");
 console.log('Database connected successfully ');
 console.log(connect.connection.host);
     }
