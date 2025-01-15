5f6078881e1197e463584e4c6b0e22d143614cca
iqra <im4356927@gmail.com>
2025-01-15 14:39:08 -0800

    done

---
+++ api/schema/message.schema.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+export const  message = Schema({
+content:{
+    type:String,
+},
+attachment:[
+    {
+        public_id : String,
+        url:String
+    }
+],
+sender:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref:"User",
+    required:true
+},
+chatId:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref:"Chat",
+    required:true
+}
+
+},{timestamps: true}) ;
+export const Message = mongoose.model("Message",message) 
